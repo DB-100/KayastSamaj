76aae8fc12954c8d649ae47648ed58e3656b5697
DB-100 <90266223+DB-100@users.noreply.github.com>
2024-07-19 14:51:42 +0530

    Add files via upload

---
+++ kayastSamaj/src/main/java/com/example/kayastSamaj/KayastSamajApplication.java
@@ -0,0 +1,13 @@
+package com.example.kayastSamaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KayastSamajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KayastSamajApplication.class, args);
+	}
+
+}
---
+++ kayastSamaj/src/test/java/com/example/kayastSamaj/KayastSamajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kayastSamaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KayastSamajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
