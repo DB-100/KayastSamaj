ffc329dff801a34107b95dfe543185e8b8912eba
Astitva <deshpandeastitva@gmail.com>
2024-07-19 16:59:42 +0530

    hii

--- kayastSamaj/src/main/java/com/example/kayastSamaj/Entity/AdminEntity.java
+++ kayastSamaj/src/main/java/com/example/kayastSamaj/Entity/AdminEntity.java
@@ -1,10 +1,13 @@
 package com.example.kayastSamaj.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table
 public class AdminEntity {
 
+@Id
+private int name;
 }
